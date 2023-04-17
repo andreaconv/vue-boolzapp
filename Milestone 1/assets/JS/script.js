3531f41e284c64e2b666bbd78325a3e38fef8b4d
andreaconv <andrea2convertini@gmail.com>
2023-04-17 18:34:59 +0200

    [edit] aside contatti aggiunti dinamicamente

--- Milestone 1/assets/JS/script.js
+++ Milestone 1/assets/JS/script.js
@@ -8,7 +8,7 @@ createApp({
       contacts: [
         {
           name: 'Michele',
-          avatar: './img/avatar_1.png',
+          avatar: 'assets/IMG/avatar/avatar_1.jpg',
           visible: true,
           messages: [
             {
@@ -30,7 +30,7 @@ createApp({
         },
         {
           name: 'Fabio',
-          avatar: './img/avatar_2.png',
+          avatar: 'assets/IMG/avatar/avatar_2.jpg',
           visible: true,
           messages: [
             {
@@ -52,7 +52,7 @@ createApp({
         },
         {
           name: 'Samuele',
-          avatar: './img/avatar_3.png',
+          avatar: 'assets/IMG/avatar/avatar_3.jpg',
           visible: true,
           messages: [
             {
@@ -74,7 +74,7 @@ createApp({
         },
         {
           name: 'Alessandro B.',
-          avatar: './img/avatar_4.png',
+          avatar: 'assets/IMG/avatar/avatar_4.jpg',
           visible: true,
           messages: [
             {
@@ -91,7 +91,7 @@ createApp({
         },
         {
           name: 'Alessandro L.',
-          avatar: './img/avatar_5.png',
+          avatar: 'assets/IMG/avatar/avatar_5.jpg',
           visible: true,
           messages: [
             {
@@ -108,7 +108,7 @@ createApp({
         },
         {
           name: 'Claudia',
-          avatar: './img/avatar_5.png',
+          avatar: 'assets/IMG/avatar/avatar_6.jpg',
           visible: true,
           messages: [
             {
@@ -130,7 +130,7 @@ createApp({
         },
         {
           name: 'Federico',
-          avatar: './img/avatar_7.png',
+          avatar: 'assets/IMG/avatar/avatar_7.jpg',
           visible: true,
           messages: [
             {
@@ -147,7 +147,7 @@ createApp({
         },
         {
           name: 'Davide',
-          avatar: './img/avatar_8.png',
+          avatar: 'assets/IMG/avatar/avatar_8.jpg',
           visible: true,
           messages: [
             {
