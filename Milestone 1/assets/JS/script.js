7a9d1a082f9d805d104c4437c45e878b2c385e9e
andreaconv <andrea2convertini@gmail.com>
2023-04-17 11:02:46 +0200

    [add] scaffolding iniziale

---
+++ Milestone 1/assets/JS/script.js
@@ -0,0 +1,21 @@
+const {createApp} = Vue;
+
+createApp({
+
+  data(){
+
+    return{
+      
+    }
+
+  },
+
+  methods:{
+    //FUNZIONI
+  },
+
+  mounted() {
+    console.log("vue funziona")
+  }
+
+}).mount('#app')
